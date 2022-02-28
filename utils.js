39b9ba5e95610da5f7e38cf9a211bda04a4d1784
jozdk <djannikhaezo@web.de>
2022-02-28 15:26:43 +0100

    Change sendFile function to read files synchronously

--- utils.js
+++ utils.js
@@ -3,12 +3,22 @@ const fs = require("fs"),
 
 module.exports = {
     sendFile: (file, res) => {
-        fs.readFile(file, (error, data) => {
-            if (error) {
-                res.writeHead(500, contentTypes.html);
-                res.end("<h1>There was an error on the server. Try again or contact site administrators.</h1>");
-            }
+        try {
+            const data = fs.readFileSync(file);
             res.end(data);
-        });
+        } catch (err) {
+            res.writeHead(500, contentTypes[".html"]);
+            res.end("<h1>There was an error on the server. Try again or contact site administrators.</h1>");
+        }
+
+        // // Asynchronous
+        
+        // fs.readFileSync(file, (error, data) => {
+        //     if (error) {
+        //         res.writeHead(500, contentTypes.html);
+        //         res.end("<h1>There was an error on the server. Try again or contact site administrators.</h1>");
+        //     }
+        //     res.end(data);
+        // });
     }
 }
